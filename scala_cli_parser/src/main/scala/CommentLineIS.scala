c6e5e99e026e0958a04e047e30c70413090810d2
Felipe M. Vieira <fmv1992@gmail.com>
2021-01-02 18:25:38 -0300

    added exact same creation signature on companion object

--- scala_cli_parser/src/main/scala/CommentLineIS.scala
+++ scala_cli_parser/src/main/scala/CommentLineIS.scala
@@ -1,14 +1,14 @@
 package fmv1992.scala_cli_parser
 
-case class CommentLineIS(accumulated: Seq[Char])
+case class CommentLineIS private (accumulated: Seq[Char])
     extends ParsedIntermediateState[Char, Map[String, String]] {
 
-  // def apply[A, B, C[A, B] <: ParsedIntermediateState[A, B]](
+  // // def apply[A, B, C[A, B] <: ParsedIntermediateState[A, B]](
+  // //     i: Seq[Char]
+  // // ): C = CommentLineIS(i.toSeq)
+  // def apply[C >: ParsedIntermediateState[Char, Map[String, String]]](
   //     i: Seq[Char]
-  // ): C = CommentLineIS(i.toSeq)
-  def apply[C >: ParsedIntermediateState[Char, Map[String, String]]](
-      i: Seq[Char]
-  ): ParsedIntermediateState[Char, Map[String, String]] = CommentLineIS(i.toSeq)
+  // ): ParsedIntermediateState[Char, Map[String, String]] = CommentLineIS(i.toSeq)
 
   def getFirstSignificantCharInLastLine: Option[Char] = {
     val newlinePos = accumulated.lastIndexOf('\n')
@@ -33,5 +33,23 @@ case class CommentLineIS(accumulated: Seq[Char])
     val (valid, invalid) = accumulated.splitAt(newlineAfterLastCommentLastPos)
     (CommentLineIS(valid), invalid)
   }
+
+}
+
+object CommentLineIS {
+
+  // [error] /home/monteirobd/dev/pud/_other/not_yet_commited_projects/scala_cli_parser/scala_cli_parser/src/main/scala/CommentLineIS.scala:34:6: ambiguous reference to overloaded definition,
+  // [error] both method apply in object CommentLineIS of type (accumulated: Seq[Char]): fmv1992.scala_cli_parser.CommentLineIS
+  // [error] and  method apply in object CommentLineIS of type [C >: fmv1992.scala_cli_parser.ParsedIntermediateState[Char,Map[String,String]]](i: Seq[Char]): C
+  // [error] match argument types (Seq[Char]) and expected result type fmv1992.scala_cli_parser.ParsedIntermediateState[Char,Map[String,String]]
+  // [error]     (CommentLineIS(valid), invalid)
+  // [error]      ^
+
+  // def apply[A, B, C[A, B] <: ParsedIntermediateState[A, B]](
+  //     i: Seq[Char]
+  // ): C = CommentLineIS(i.toSeq)
+  def apply[C >: ParsedIntermediateState[Char, Map[String, String]]](
+      i: Seq[Char]
+  ): C = CommentLineIS(i.toSeq)
 
 }
