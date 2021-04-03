340f4017af41b7874ca85b80e323facc3f036f3e
Felipe M. Vieira <fmv1992@gmail.com>
2021-04-03 14:34:31 +0000

    added commentaries

--- scala_cli_parser/.jvm/src/test/scala/TestParseExceptionJVM.scala
+++ scala_cli_parser/.jvm/src/test/scala/TestParseExceptionJVM.scala
@@ -1,6 +1,5 @@
 package fmv1992.scala_cli_parser
 
-
 class TestParseExceptionJVM extends TestParseException {
 
   test("ParseException name matches (exact): 01.") {
@@ -15,6 +14,9 @@ class TestParseExceptionJVM extends TestParseException {
       ParseException.getExceptionMessage(
         input02.toSeq,
         SpaceConfParser
+        // Differs from
+        // `scala_cli_parser:15d3f98:scala_cli_parser/.native/src/test/scala/TestParseExceptionNative.scala:18`
+        // by a `$` sign.
       ) === "'SpaceConfParser$': 'ErrorPositionExisting(0,10,11)': 'x     '."
     )
   }
