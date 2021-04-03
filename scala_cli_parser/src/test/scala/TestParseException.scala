15d3f98887d40e399497f7509030c23cf8963e24
Felipe M. Vieira <fmv1992@gmail.com>
2021-04-03 14:32:04 +0000

    divide `TestParseException` into JVM/Scala Native

--- scala_cli_parser/src/test/scala/TestParseException.scala
+++ scala_cli_parser/src/test/scala/TestParseException.scala
@@ -5,34 +5,20 @@ import org.scalatest.funsuite.AnyFunSuite
 
 class TestParseException extends AnyFunSuite {
 
-  test("`CommentConfParser` invalid throws ParseException.") {
+  val input01 = "abc"
+  val input02 = "" * 10 + "x" + " " * 10
 
+  val parseException = CommentConfParser.parse(input01).left.value
+
+  test("`CommentConfParser` invalid throws ParseException.") {
     // ???: Revert adding blinking
     // `scala_cli_parser:b45d9a7:scala_cli_parser/src/main/scala/ParseException.scala:15`
     // to exceptions.
-
-    val input01 = "abc"
-    val input02 = "" * 10 + "x" + " " * 10
-
-    val parseException = CommentConfParser.parse(input01).left.value
     assert(
       parseException.isInstanceOf[ParseException]
     )
     assert(
       parseException.getMessage === "ErrorPositionExisting(0,0,1)"
-    )
-    assert(
-      ParseException.getExceptionMessage(
-        input01.toSeq,
-        CommentConfParser
-      ) === "'CommentConfParser$': 'ErrorPositionExisting(0,0,1)': 'abc'."
-    )
-
-    assert(
-      ParseException.getExceptionMessage(
-        input02.toSeq,
-        SpaceConfParser
-      ) === "'SpaceConfParser$': 'ErrorPositionExisting(0,10,11)': 'x     '."
     )
   }
 
