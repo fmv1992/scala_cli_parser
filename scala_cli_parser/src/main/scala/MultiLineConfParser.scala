61ad016e9a3aaae7f533df14b1ebd8179fd0931c
Felipe M. Vieira <felipev@telnyx.com>
2021-07-17 16:44:37 -0300

    got '`MultiLineConfParser.transform` valid.' right

--- scala_cli_parser/src/main/scala/MultiLineConfParser.scala
+++ scala_cli_parser/src/main/scala/MultiLineConfParser.scala
@@ -1,132 +1,176 @@
-// package fmv1992.scala_cli_parser
+package fmv1992.scala_cli_parser
+
+import scala.util.Failure
+import scala.util.Success
+import scala.util.Try
+
+/** Parse a **single** config that spans multiple lines.
+  *  It **must** span more than 1 line.
+  */
+object MultiLineConfParser
+    extends ParserPartial[
+      Seq[Char],
+      Try[ParsedResult[Seq[Char], Map[String, String]]]
+    ]
+    with ParserWithTry[
+      Seq[Char],
+      ParsedResult[Seq[Char], Map[String, String]]
+    ] {
+
+  override def parse(
+      input: Seq[Char]
+  ): Try[ParsedResult[Seq[Char], Map[String, String]]] = ???
+
+  def partialParse(
+      input: Seq[Char]
+  ): (Seq[Char], Try[ParsedResult[Seq[Char], Map[String, String]]]) = ???
+
+  def transform(
+      input: Seq[Char]
+  ): ParsedResult[Seq[Char], Map[String, String]] = {
+    val lines = splitOnLines(input)
+    // Get key.
+    val keyPos = lines.head.indexOf(':')
+    val key =
+      if (keyPos == -1) {
+        throw new ParseException(
+          List("Char ':' not found:", input.mkString).mkString("\n")
+        )
+      } else {
+        lines.head.take(keyPos)
+      }
+
+    // Get value.
+    val pipePos = lines.map(_.indexOf('|'))
+    val pipePosAreTheSame: Boolean = pipePos.forall(_ == pipePos.head)
+    val value =
+      if (pipePosAreTheSame) {
+        lines.map(x => x.drop(pipePos.head + 1).mkString.strip).mkString("\n")
+      } else {
+        throw new ParseException(input.mkString)
+      }
+    ParsedResult(input, Map(key.mkString -> value))
+  }
+
+  private def splitOnLines(input: Seq[Char]): Seq[Seq[Char]] = {
+    input
+      .foldLeft(
+        (LazyList.empty: LazyList[Seq[Char]], Seq.empty: Seq[Char])
+      )((acc, char) => {
+        val accll = acc._1
+        val accseq = acc._2
+        if (char == '\n') {
+          (accll.appended(accseq.appended('\n')), Seq.empty)
+        } else {
+          (accll, accseq.appended(char))
+        }
+      }) match {
+      case (l, Seq()) => l
+      case (l, s)     => l.appended(s)
+    }
+  }
 //
-// /** Parse a **single** config that spans multiple lines.
-//   */
-// object MultiLineConfParser
-//     extends ParserWithEither[
-//       Seq[Char],
-//       ParsedResult[Seq[Char], Map[String, String]]
-//     ] {
+//  private val SpacedSolidLineStartingWithPipe
+//      : ParserWithEither[Seq[Char], ParsedResult[Seq[Char], String]] =
+//    ParserUtils.many(
+//      ParserUtils.and(
+//        SpaceConfParser,
+//        SolidLineStartingWithPipe,
+//        (
+//            x: ParsedResult[Seq[Char], Map[String, String]],
+//            y: ParsedResult[Seq[Char], String]
+//        ) => ParsedResult(x.data ++ y.data, y.result)
+//      ),
+//      (
+//          x: ParsedResult[Seq[Char], String],
+//          y: ParsedResult[Seq[Char], String]
+//      ) => ParsedResult(x.data ++ y.data, x.result ++ y.result)
+//    )
 //
-//   private def splitOnLines(input: Seq[Char]): Seq[Seq[Char]] = {
-//     input
-//       .foldLeft(
-//         (LazyList.empty: LazyList[Seq[Char]], Seq.empty: Seq[Char])
-//       )((acc, char) => {
-//         val accll = acc._1
-//         val accseq = acc._2
-//         if (char == '\n') {
-//           (accll.appended(accseq.appended('\n')), Seq.empty)
-//         } else {
-//           (accll, accseq.appended(char))
-//         }
-//       }) match {
-//       case (l, Seq()) => l
-//       case (l, s)     => l.appended(s)
-//     }
-//   }
+//  def transform(
+//      input: Seq[Char]
+//  ): ParsedResult[Seq[Char], Map[String, String]] = {
+//    throw new Exception(input.toString)
+//  }
 //
-//   private val SpacedSolidLineStartingWithPipe
-//       : ParserWithEither[Seq[Char], ParsedResult[Seq[Char], String]] =
-//     ParserUtils.many(
-//       ParserUtils.and(
-//         SpaceConfParser,
-//         SolidLineStartingWithPipe,
-//         (
-//             x: ParsedResult[Seq[Char], Map[String, String]],
-//             y: ParsedResult[Seq[Char], String]
-//         ) => ParsedResult(x.data ++ y.data, y.result)
-//       ),
-//       (
-//           x: ParsedResult[Seq[Char], String],
-//           y: ParsedResult[Seq[Char], String]
-//       ) => ParsedResult(x.data ++ y.data, x.result ++ y.result)
-//     )
+//  def isValid(input: Seq[Char]) = {
+//    val splitLines = splitOnLines(input)
+//    val firstLine: Seq[Char] = splitLines.head
+//    val otherLines: Seq[Seq[Char]] = splitLines.tail
+//    require(
+//      (firstLine.toList +: otherLines.toList).flatten == input.toList,
+//      (firstLine.toList, otherLines.toList, input.toList)
+//    )
+//    val pipePos = splitLines.map(_.indexOf('|'))
+//    val pipePosAreTheSame: Boolean = pipePos.forall(_ == pipePos.head)
+//    Thread.sleep(1000)
+//    Console.err.println("-" * 79)
+//    Console.err.println(SingleLineConfParser.isValid(firstLine))
+//    Console.err.println(pipePosAreTheSame)
+//    Console.err.println(
+//      otherLines
+//        .forall(
+//          SpacedSolidLineStartingWithPipe.isValid(_)
+//        )
+//    )
+//    Console.err.println("-" * 79)
+//    SingleLineConfParser.isValid(firstLine) && pipePosAreTheSame &&
+//    otherLines
+//      .forall(
+//        SpacedSolidLineStartingWithPipe.isValid(_)
+//      )
+//  }
 //
-//   def transform(
-//       input: Seq[Char]
-//   ): ParsedResult[Seq[Char], Map[String, String]] = {
-//     throw new Exception(input.toString)
-//   }
+//  def getValidSubSequence(input: Seq[Char]): Option[Seq[Char]] = {
+//    // Console.err.println("-" * 79)
+//    // Console.err.println("🐛" + input.mkString + "🐛")
+//    // Console.err.println("-" * 79)
+//    val splitLines = splitOnLines(input)
+//    val idx = splitLines.indexWhere(!_.contains('|'))
+//    if (idx == -1) {
+//      if (isValid(input)) {
+//        Some(input)
+//      } else {
+//        ???
+//      }
+//    } else {
+//      val subLines = splitLines.slice(0, idx)
+//      val subStringNew = subLines.mkString("\n")
+//      if (isValid(subStringNew)) {
+//        Some(subStringNew)
+//      } else {
+//        ???
+//      }
+//    }
+//  }
 //
-//   def isValid(input: Seq[Char]) = {
-//     val splitLines = splitOnLines(input)
-//     val firstLine: Seq[Char] = splitLines.head
-//     val otherLines: Seq[Seq[Char]] = splitLines.tail
-//     require(
-//       (firstLine.toList +: otherLines.toList).flatten == input.toList,
-//       (firstLine.toList, otherLines.toList, input.toList)
-//     )
-//     val pipePos = splitLines.map(_.indexOf('|'))
-//     val pipePosAreTheSame: Boolean = pipePos.forall(_ == pipePos.head)
-//     Thread.sleep(1000)
-//     Console.err.println("-" * 79)
-//     Console.err.println(SingleLineConfParser.isValid(firstLine))
-//     Console.err.println(pipePosAreTheSame)
-//     Console.err.println(
-//       otherLines
-//         .forall(
-//           SpacedSolidLineStartingWithPipe.isValid(_)
-//         )
-//     )
-//     Console.err.println("-" * 79)
-//     SingleLineConfParser.isValid(firstLine) && pipePosAreTheSame &&
-//     otherLines
-//       .forall(
-//         SpacedSolidLineStartingWithPipe.isValid(_)
-//       )
-//   }
+//}
 //
-//   def getValidSubSequence(input: Seq[Char]): Option[Seq[Char]] = {
-//     // Console.err.println("-" * 79)
-//     // Console.err.println("🐛" + input.mkString + "🐛")
-//     // Console.err.println("-" * 79)
-//     val splitLines = splitOnLines(input)
-//     val idx = splitLines.indexWhere(!_.contains('|'))
-//     if (idx == -1) {
-//       if (isValid(input)) {
-//         Some(input)
-//       } else {
-//         ???
-//       }
-//     } else {
-//       val subLines = splitLines.slice(0, idx)
-//       val subStringNew = subLines.mkString("\n")
-//       if (isValid(subStringNew)) {
-//         Some(subStringNew)
-//       } else {
-//         ???
-//       }
-//     }
-//   }
+//object SolidLineStartingWithPipe
+//    extends ParserWithEither[Seq[Char], ParsedResult[Seq[Char], String]] {
 //
-// }
+//  def transform(
+//      input: Seq[Char]
+//  ): ParsedResult[Seq[Char], String] = {
+//    ParsedResult(input, input.tail.mkString)
+//  }
 //
-// object SolidLineStartingWithPipe
-//     extends ParserWithEither[Seq[Char], ParsedResult[Seq[Char], String]] {
+//  def isValid(input: Seq[Char]) = {
+//    lazy val notEmpty = !input.isEmpty
+//    lazy val isPipeFirst = input.head == '|'
+//    lazy val newLinePos = input.indexOf('\n')
+//    lazy val newLinesOnlyAtEnd =
+//      (newLinePos == -1) || (newLinePos == input.length - 1)
+//    notEmpty && isPipeFirst && newLinesOnlyAtEnd
+//  }
 //
-//   def transform(
-//       input: Seq[Char]
-//   ): ParsedResult[Seq[Char], String] = {
-//     ParsedResult(input, input.tail.mkString)
-//   }
-//
-//   def isValid(input: Seq[Char]) = {
-//     lazy val notEmpty = !input.isEmpty
-//     lazy val isPipeFirst = input.head == '|'
-//     lazy val newLinePos = input.indexOf('\n')
-//     lazy val newLinesOnlyAtEnd =
-//       (newLinePos == -1) || (newLinePos == input.length - 1)
-//     notEmpty && isPipeFirst && newLinesOnlyAtEnd
-//   }
-//
-//   def getValidSubSequence(input: Seq[Char]): Option[Seq[Char]] = {
-//     val newLinePos = input.indexOf('\n')
-//     val line = if (newLinePos == -1) input else input.slice(0, newLinePos)
-//     if (isValid(line)) {
-//       Some(line)
-//     } else {
-//       None
-//     }
-//   }
-// }
+//  def getValidSubSequence(input: Seq[Char]): Option[Seq[Char]] = {
+//    val newLinePos = input.indexOf('\n')
+//    val line = if (newLinePos == -1) input else input.slice(0, newLinePos)
+//    if (isValid(line)) {
+//      Some(line)
+//    } else {
+//      None
+//    }
+//  }
+}
--- scala_cli_parser/src/test/scala/TestMultiLineConfParser.scala
+++ scala_cli_parser/src/test/scala/TestMultiLineConfParser.scala
@@ -1,45 +1,48 @@
-// // project scala_cli_parserCrossProjectJVM;~testOnly fmv1992.scala_cli_parser.TestMultiLineConfParser
-//
-// package fmv1992.scala_cli_parser
-//
-// import org.scalatest.concurrent.TimeLimits
-// import org.scalatest.funsuite.AnyFunSuite
-// import org.scalatest.time.Millis
-// import org.scalatest.time.Span
-//
-// class TestMultiLineConfParser extends AnyFunSuite with TimeLimits {
-//
-//   val valid01 = """
-// help: | cliarg
-//       |
-//       | other line
-// """.trim
-//
-//   val inValid01 = """
-// help: | cliarg
-//      |
-//       | other line
-// """.trim
-//
-//   test("`MultiLineConfParser` valid.")(failAfter(Span(200000, Millis))({
-//     assert(
-//       MultiLineConfParser.isValid(valid01)
-//     )
-//   }))
-//
-//   // test("`MultiLineConfParser` invalid.")(failAfter(Span(200000, Millis))({
-//   // assert(
-//   // !MultiLineConfParser.isValid(inValid01)
-//   // )
-//   // }))
-//
-//   ignore("`ConfParser` full example.")(failAfter(Span(500, Millis))({
-//     val fullConfig =
-//       scala.io.Source
-//         .fromResource("test_multiline_01.txt")
-//         .getLines()
-//         .mkString("\n")
-//     assert(ConfParser.parse(fullConfig) === Map.empty)
-//   }))
-//
-// }
+// project scala_cli_parserCrossProjectJVM;~testOnly fmv1992.scala_cli_parser.TestMultiLineConfParser
+
+package fmv1992.scala_cli_parser
+
+import org.scalatest.concurrent.TimeLimits
+import org.scalatest.funsuite.AnyFunSuite
+import org.scalatest.time.Millis
+import org.scalatest.time.Span
+
+class TestMultiLineConfParser extends AnyFunSuite with TimeLimits {
+
+  val valid01 = """
+help: | cliarg
+      |
+      | other line
+ """.trim
+
+  val inValid01 = """
+ help: | cliarg
+      |
+       | other line
+ """.trim
+
+  test("`MultiLineConfParser.transform` valid.")(
+    failAfter(Span(200000, Millis))({
+      assert(
+        ParsedResult(valid01.toSeq, Map("help" -> "cliarg\n\nother line")) ===
+          MultiLineConfParser.transform(valid01)
+      )
+    })
+  )
+
+  // test("`MultiLineConfParser` invalid.")(failAfter(Span(200000, Millis))({
+  // assert(
+  // !MultiLineConfParser.isValid(inValid01)
+  // )
+  // }))
+
+  // ignore("`ConfParser` full example.")(failAfter(Span(500, Millis))({
+  //   val fullConfig =
+  //     scala.io.Source
+  //       .fromResource("test_multiline_01.txt")
+  //       .getLines()
+  //       .mkString("\n")
+  //   // assert(ConfParser.parse(fullConfig) === Map.empty)
+  // }))
+
+}
