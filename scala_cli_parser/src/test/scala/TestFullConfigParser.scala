253e5135453002e7fce30b41e201ca7ba455bf72
Felipe M. Vieira <felipev@telnyx.com>
2021-07-25 10:49:58 -0300

    added more notes on wrong package mixin

--- scala_cli_parser/src/test/scala/TestFullConfigParser.scala
+++ scala_cli_parser/src/test/scala/TestFullConfigParser.scala
@@ -2,7 +2,11 @@ package fmv1992.scala_cli_parser.test
 
 import scala.util.Success
 
+// ???: This in all `Test*` classes is a remnant of
+// <https://github.com/fmv1992/scala_cli_parser/blob/9cbbb97bae6e19f4f7b54ae9f3d7a6075f5868ce/scala_cli_parser/src/test/scala/package.scala#L8>
+// this limitation.
 import fmv1992.scala_cli_parser._
+
 import org.scalatest.concurrent.TimeLimits
 import org.scalatest.funsuite.AnyFunSuite
 import org.scalatest.time.Millis
