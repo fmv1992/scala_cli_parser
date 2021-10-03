2f3380e1a546742fdc18f413c7de912a34c02b4e
Felipe M. Vieira <felipev@telnyx.com>
2021-10-03 19:38:06 -0300

    add a `Main` class for this package

---
+++ scala_cli_parser/src/main/scala/Main.scala
@@ -0,0 +1,26 @@
+package fmv1992.scala_cli_parser
+
+import fmv1992.fmv1992_scala_utilities.util.S
+import fmv1992.scala_cli_parser.cli.ArgumentCLI
+import fmv1992.scala_cli_parser.conf.ParserConfigFile
+import fmv1992.scala_cli_parser.util.MainTestableConfBased
+
+object Main extends MainTestableConfBased {
+
+  val programName: String = "scala_cli_parser"
+
+  val version: String = S.putfile("./src/main/resources/version")
+
+  val CLIConfigContents =
+    S.putfile("./src/main/resources/scala_cli_parser_config.conf")
+
+  def testableMain(args: Set[ArgumentCLI]): Iterable[String] = {
+    ParserConfigFile.parse(
+      LazyList
+        .continually(scala.io.StdIn.readLine())
+        .takeWhile(_ != null)
+        .mkString("\n")
+    )
+    Seq.empty
+  }
+}
